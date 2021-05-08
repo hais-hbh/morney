94f86b209c4e744383c86d1c7e4dc513a53ac56f
hais-hbh <578092215@qq.com>
2021-05-08 17:34:03 +0800

    基本完成Tags组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -2,20 +2,44 @@
   <div class="tags">
 
     <div class="new">
-      <button>新增标签</button>
+      <button @click="add">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+      @click="toggle(tag)"
+      :class="{selected:selectedTags.indexOf(tag)>=0}">
+        {{tag}}
+      </li>
+
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Tags"
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue{
+  @Prop(Array) readonly dataSource: string[]|undefined;
+  selectedTags: string[] = [];
+  add(){
+    const tagName = window.prompt('请输入标签名')
+   if(tagName === ''){
+     window.alert('标签名不能为空')
+   }else if(this.dataSource){
+      this.$emit('update:dataSource',[...this.dataSource,tagName])
+   }
+  }
+  toggle(tag:string){
+    const index = this.selectedTags.indexOf(tag)
+    if(index>=0){
+      this.selectedTags.splice(index,1)
+    }else{
+      this.selectedTags.push(tag)
+    }
+
+  }
 }
 </script>
 
@@ -32,7 +56,8 @@ name: "Tags"
     flex-wrap: wrap;
 
     > li {
-      background: #b9b9b9;
+      $bg:#d9d9d9;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -40,6 +65,10 @@ name: "Tags"
       margin-right: 12px;
       border-radius: $h/2;
       margin-top: 4px;
+      &.selected{
+        background: darken($bg,50%);
+        color: white;
+      }
     }
   }
 
